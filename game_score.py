46a74da36356e9f8caba02881bf0c41ea5c1cae4
DevStudent2601048 <student2601048@university.edu>
2026-02-07 20:52:46 +0530

    feat: add Player score calculator

---
+++ game_score.py
@@ -0,0 +1,15 @@
+
+# Player Score Calculator
+# Input Collection
+Player_Name = input("Enter the Player Name: ")
+# Numeric Input Processing
+Games_Played = int(input("Enter the Games Played: "))
+# Score Data Entry
+Total_Score_Achieved = int(input("Enter Total Score: "))
+# Computation
+Average_Score = Total_Score_Achieved / Games_Played
+# OutputDisplay
+print(f"\nPlayer:{Player_Name}")
+print(f"\nGames Played:{Games_Played}")
+print(f"\nTotal Score:{Total_Score_Achieved}")
+print(f"\nAvergae Score:{Average_Score}")
